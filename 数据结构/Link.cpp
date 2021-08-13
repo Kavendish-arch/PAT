74e0c70312099be404ec8f1385a29a3201a84f93
Kavendish-arch <chen_yingtao@outlook.com>
2021-08-13 23:35:59 +0800

    链表的实现

--- 数据结构/Link.cpp
+++ 数据结构/Link.cpp
@@ -1,18 +1,19 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-typedef struct Link
+typedef struct _node
 {
-	int value;
-	struct Link * next;
-}Node;
+    int value;
+    struct _node * next;  
+} Node;
 
-int replace(Node * a, Node * b){
-	Node tmp = *a;
-	*a = *b;
-	*b = tmp;
-	return 0;
-}
+typedef struct _link
+{
+    Node * head;
+    Node * tail;
+}Link;
+
+// 传入的结构体指针 是结构体变量
 
 void append(Node * head, int data){
 	Node *p = (Node*) malloc (sizeof(Node));
@@ -29,31 +30,7 @@ void append(Node * head, int data){
 		head = p;
 	}
 }
-int main(int argc, char const *argv[])
-{
-	Node * head = NULL;
-	append(head, 20);
-	
-	printf("%d", head->value);
-	return 0;
-}
 
-
-#include <stdio.h>
-#include <stdlib.h>
-typedef struct _node
-{
-    int value;
-    struct _node * next;  
-} Node;
-
-typedef struct _link
-{
-    Node * head;
-    Node * tail;
-}Link;
-
-// 传入的结构体指针 是结构体变量
 /**
  * 全局变量
  * 返回值
@@ -88,12 +65,25 @@ void node_append(Link * link_node, int data) {
         // head = p;
     // }
 }
+
 void traverse(Link * p_head){
     for (Node *p = p_head->head; p; p=p->next){
         printf("value = %d addr = %p\n", p->value, p);
     }
 }
+/**
+ * 头插法 单链表
+ */
+void insert_from_head(Link * p_head, int data){
+	//建构结点
+	Node * p = (Node * ) malloc(sizeof(Node));
+    p->value = data;
 
+	//结点下一个是原头节点
+	p->next = p_head->head;
+	// 头节点 变成 新结点
+	p_head->head = p;
+}
 void insert(Link * p_head, int data, int index){
     Node * tail = p_head->head;
     for(int i = 1; i < index; i ++){
@@ -103,7 +93,7 @@ void insert(Link * p_head, int data, int index){
     p->value = data;
 
     p->next = tail->next;
-    tail->next = p;
+    tail->next =    p;
 }
 void remove(Link * p_head, int * data, int index){
     Node * tail = p_head->head;
@@ -115,6 +105,33 @@ void remove(Link * p_head, int * data, int index){
     tail->next = p->next;
     free(p);
 }
+// 查找 elem 头节点存数字
+void get_elem(Link * p_head, Node * data, int index){
+	
+	//tail 头结点 
+    Node * tail = p_head->head;
+
+    for(int i = 1; i <= index; i ++){
+        tail = tail->next;
+    }
+    data = tail;
+}
+void locate_elem(Link * p_head, int *index, int targe){
+	Node * tail = p_head->head;
+	int i = 0;
+	while (tail != NULL && tail->value != targe)
+	{
+		tail = tail->next;
+		i++;
+
+		// if (tail->value != targe){
+		// } else {
+			// *index = i;
+			// return;
+		// }
+	}	
+	*index = i;
+}
 int main(int argc, char const *argv[])
 {
     Link l_head;
@@ -124,28 +141,41 @@ int main(int argc, char const *argv[])
     // tmp->value = 20;
     
     // l_head.head = tmp;
-    printf("%d", sizeof(Node));
+    printf("%d\n", sizeof(Node));
     l_head.head = l_head.tail = NULL;
     // Node * head = NULL;
     node_append(&l_head, 2);
     node_append(&l_head, 5);
     node_append(&l_head, 6);
+	insert_from_head(&l_head, 9);
     traverse(&l_head);
-    printf("\n");
-    insert(&l_head, 10, 2-1);
-    traverse(&l_head);
-    printf("\n");
-    insert(&l_head, 30, 2-1);
-    traverse(&l_head);
-    printf("\n");
-    int x = 0;
-    remove(&l_head, &x, 2-1);
-    printf("delete %d\n",x);
+    
+	printf("\n");
+    insert(&l_head, 10, 2);
     traverse(&l_head);
 
+    printf("\n");
+    insert(&l_head, 30, 2);
+    traverse(&l_head);
+
+    int x = 0;
+    remove(&l_head, &x, 2);
+    printf("\ndelete %d\n",x);
+    traverse(&l_head);
+    
+	Node y;
+    get_elem(&l_head, &y, 1);
+	printf("\nread 2 num %d\n",y.value);
+    // traverse(&l_head);
+	int z = 0;
+	locate_elem(&l_head, &z, 9);
+	printf("locate elem = %d\n", z), 
+	locate_elem(&l_head, &z, 10);
+	printf("locate elem = %d\n", z), 
+
+
+	printf("\nhead and tail\n");
     printf("head = %d\n", l_head.head->value);
-    printf("p + 0 = %d\n", (l_head.head+0)->value);
-    printf("p + 1 = %d\n", (l_head.head+1)->value);
     printf("tail = %d\n", l_head.tail->value);
     return 0;
 }
