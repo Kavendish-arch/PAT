2697c31209132a6b4a507f7b73d82c803351c1e4
chen_yingtao <55643805+Kavendish-arch@users.noreply.github.com>
2021-08-13 16:32:25 +0800

    Update Link.cpp

--- 数据结构/Link.cpp
+++ 数据结构/Link.cpp
@@ -37,3 +37,115 @@ int main(int argc, char const *argv[])
 	printf("%d", head->value);
 	return 0;
 }
+
+
+#include <stdio.h>
+#include <stdlib.h>
+typedef struct _node
+{
+    int value;
+    struct _node * next;  
+} Node;
+
+typedef struct _link
+{
+    Node * head;
+    Node * tail;
+}Link;
+
+// 传入的结构体指针 是结构体变量
+/**
+ * 全局变量
+ * 返回值
+ */
+// void node_create(Node * head, int data) {
+void node_append(Link * link_node, int data) {
+
+    Node * tail = link_node->head;
+
+    Node * p = (Node * ) malloc (sizeof(Node));
+    p->value = data;
+    p->next = NULL;
+
+    if (tail){
+        while (tail -> next){
+            tail = tail->next;
+        }
+        tail->next = p;
+        link_node->tail = p;
+    } else {
+        link_node->head = p;
+    }
+    // if (link_node->tail) {
+        // while (tail -> next)
+        // {
+            // tail = tail->next;
+        // }
+        // tail->next = p; 
+    // }
+    // else {
+        // link_node->tail=p;    
+        // head = p;
+    // }
+}
+void traverse(Link * p_head){
+    for (Node *p = p_head->head; p; p=p->next){
+        printf("value = %d addr = %p\n", p->value, p);
+    }
+}
+
+void insert(Link * p_head, int data, int index){
+    Node * tail = p_head->head;
+    for(int i = 1; i < index; i ++){
+        tail = tail->next;
+    }
+    Node * p = (Node * ) malloc(sizeof(Node));
+    p->value = data;
+
+    p->next = tail->next;
+    tail->next = p;
+}
+void remove(Link * p_head, int * data, int index){
+    Node * tail = p_head->head;
+    for(int i = 1; i < index; i ++){
+        tail = tail->next;
+    }
+    Node * p = tail->next; 
+    *data = p->value;
+    tail->next = p->next;
+    free(p);
+}
+int main(int argc, char const *argv[])
+{
+    Link l_head;
+
+    // Node * tmp = (Node *) malloc (sizeof(Node));
+    // tmp->next = NULL;
+    // tmp->value = 20;
+    
+    // l_head.head = tmp;
+    printf("%d", sizeof(Node));
+    l_head.head = l_head.tail = NULL;
+    // Node * head = NULL;
+    node_append(&l_head, 2);
+    node_append(&l_head, 5);
+    node_append(&l_head, 6);
+    traverse(&l_head);
+    printf("\n");
+    insert(&l_head, 10, 2-1);
+    traverse(&l_head);
+    printf("\n");
+    insert(&l_head, 30, 2-1);
+    traverse(&l_head);
+    printf("\n");
+    int x = 0;
+    remove(&l_head, &x, 2-1);
+    printf("delete %d\n",x);
+    traverse(&l_head);
+
+    printf("head = %d\n", l_head.head->value);
+    printf("p + 0 = %d\n", (l_head.head+0)->value);
+    printf("p + 1 = %d\n", (l_head.head+1)->value);
+    printf("tail = %d\n", l_head.tail->value);
+    return 0;
+}
